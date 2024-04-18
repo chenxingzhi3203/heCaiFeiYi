71aafc0b5546a08a9ca89ac85c6f46800a4f9758
chenxingzhi123 <7350216+chenxingzhi123@user.noreply.gitee.com>
2024-04-18 23:12:36 +0800

    ceshi418

---
+++ src/assets/detailData/imgs.js
@@ -0,0 +1,264 @@
+//凤阳花鼓
+
+const img1 = require('@/assets/fengyanghuagu/huagu1.jpg')
+
+
+
+//徽墨制造
+const img11 = require('@/assets/huimozhizuo/huimozhizuo1.jpg')
+
+
+
+//界首彩陶
+const img21 = require('@/assets/jieshoucaitaozhizhuogongyi/jieshoucaitao1.jpeg')
+
+
+
+
+
+//目连戏（徽州目连戏）
+const img31 = require('@/assets/hzmlx/hzmlx1.jpg')
+const img32 = require('@/assets/hzmlx/hzmlx2.jpg')
+const img33 = require('@/assets/hzmlx/hzmlx3.jpg')
+const img34 = require('@/assets/hzmlx/hzmlx4.jpg')
+const img35 = require('@/assets/hzmlx/hzmlx5.jpg')
+const img36 = require('@/assets/hzmlx/hzmlx6.jpg')
+const img37 = require('@/assets/hzmlx/hzmlx7.jpg')
+const img38 = require('@/assets/hzmlx/hzmlx8.jpg')
+const img39 = require('@/assets/hzmlx/hzmlx9.jpg')
+const img40 = require('@/assets/hzmlx/hzmlx10.jpg')
+
+
+
+
+//徽剧（黄山市）
+const img41 = require('@/assets/hjhss/hjhss1.jpg')
+const img42 = require('@/assets/hjhss/hjhss2.jpg')
+const img43 = require('@/assets/hjhss/hjhss3.jpg')
+const img44 = require('@/assets/hjhss/hjhss4.jpg')
+const img45 = require('@/assets/hjhss/hjhss5.jpg')
+const img46 = require('@/assets/hjhss/hjhss6.jpg')
+const img47 = require('@/assets/hjhss/hjhss7.jpg')
+const img48 = require('@/assets/hjhss/hjhss8.jpg')
+const img49 = require('@/assets/hjhss/hjhss9.jpg')
+const img50 = require('@/assets/hjhss/hjhss10.jpg')
+
+
+
+
+//徽剧（省徽京剧院）
+const img51 = require('@/assets/hjshjjy/hjshjjy1.jpg')
+const img52 = require('@/assets/hjshjjy/hjshjjy2.jpg')
+const img53 = require('@/assets/hjshjjy/hjshjjy3.jpg')
+const img54 = require('@/assets/hjshjjy/hjshjjy4.jpg')
+const img55 = require('@/assets/hjshjjy/hjshjjy5.jpg')
+const img56 = require('@/assets/hjshjjy/hjshjjy6.jpg')
+const img57 = require('@/assets/hjshjjy/hjshjjy7.jpg')
+const img58 = require('@/assets/hjshjjy/hjshjjy8.jpg')
+const img59 = require('@/assets/hjshjjy/hjshjjy9.jpg')
+const img60 = require('@/assets/hjshjjy/hjshjjy10.jpg')
+
+
+
+
+//庐剧（合肥市）
+const img61 = require('@/assets/ljhfs/ljhfs1.jpg')
+const img62 = require('@/assets/ljhfs/ljhfs2.jpg')
+const img63 = require('@/assets/ljhfs/ljhfs3.jpg')
+const img64 = require('@/assets/ljhfs/ljhfs4.jpg')
+const img65 = require('@/assets/ljhfs/ljhfs5.jpg')
+const img66 = require('@/assets/ljhfs/ljhfs6.jpg')
+const img67 = require('@/assets/ljhfs/ljhfs7.jpg')
+const img68 = require('@/assets/ljhfs/ljhfs8.jpg')
+const img69 = require('@/assets/ljhfs/ljhfs9.jpg')
+const img70 = require('@/assets/ljhfs/ljhfs10.jpg')
+
+
+
+
+
+//庐剧（六安市）
+const img71 = require('@/assets/ljlas/ljlas1.jpg')
+const img72 = require('@/assets/ljlas/ljlas2.jpg')
+const img73 = require('@/assets/ljlas/ljlas3.jpg')
+const img74 = require('@/assets/ljlas/ljlas4.jpg')
+const img75 = require('@/assets/ljlas/ljlas5.jpg')
+const img76 = require('@/assets/ljlas/ljlas6.jpg')
+const img77 = require('@/assets/ljlas/ljlas7.jpg')
+const img78 = require('@/assets/ljlas/ljlas8.jpg')
+const img79 = require('@/assets/ljlas/ljlas9.jpg')
+const img80 = require('@/assets/ljlas/ljlas10.jpg')
+
+
+
+
+//泗州戏（宿州市）
+const img81 = require('@/assets/szxszs/szxszs1.jpg')
+const img82 = require('@/assets/szxszs/szxszs2.jpg')
+const img83 = require('@/assets/szxszs/szxszs3.jpg')
+const img84 = require('@/assets/szxszs/szxszs4.jpg')
+const img85 = require('@/assets/szxszs/szxszs5.jpg')
+const img86 = require('@/assets/szxszs/szxszs6.jpg')
+const img87 = require('@/assets/szxszs/szxszs7.jpg')
+const img88 = require('@/assets/szxszs/szxszs8.jpg')
+const img89 = require('@/assets/szxszs/szxszs9.jpg')
+const img90 = require('@/assets/szxszs/szxszs10.jpg')
+
+
+
+
+//巢湖民歌
+const img91 = require('@/assets/chmg/chmg1.jpg')
+const img92 = require('@/assets/chmg/chmg2.jpg')
+const img93 = require('@/assets/chmg/chmg3.jpg')
+const img94 = require('@/assets/chmg/chmg4.jpg')
+const img95 = require('@/assets/chmg/chmg5.jpg')
+const img96 = require('@/assets/chmg/chmg6.jpg')
+const img97 = require('@/assets/chmg/chmg7.jpg')
+const img98 = require('@/assets/chmg/chmg8.jpg')
+const img99 = require('@/assets/chmg/chmg9.jpg')
+const img100 = require('@/assets/chmg/chmg10.jpg')
+
+
+
+
+//当涂民歌
+const img101 = require('@/assets/dtmg/dtmg1.jpg')
+const img102 = require('@/assets/dtmg/dtmg2.jpg')
+const img103 = require('@/assets/dtmg/dtmg3.jpg')
+const img104 = require('@/assets/dtmg/dtmg4.jpg')
+const img105 = require('@/assets/dtmg/dtmg5.jpg')
+const img106 = require('@/assets/dtmg/dtmg6.jpg')
+const img107 = require('@/assets/dtmg/dtmg7.jpg')
+const img108 = require('@/assets/dtmg/dtmg8.jpg')
+const img109 = require('@/assets/dtmg/dtmg9.jpg')
+const img110 = require('@/assets/dtmg/dtmg10.jpg')
+
+
+
+
+//高腔（岳西高腔）
+const img111 = require('@/assets/gqyxgq/gqyxgq1.jpg')
+const img112 = require('@/assets/gqyxgq/gqyxgq2.jpg')
+const img113 = require('@/assets/gqyxgq/gqyxgq3.jpg')
+const img114 = require('@/assets/gqyxgq/gqyxgq4.jpg')
+const img115 = require('@/assets/gqyxgq/gqyxgq5.jpg')
+const img116 = require('@/assets/gqyxgq/gqyxgq6.jpg')
+const img117 = require('@/assets/gqyxgq/gqyxgq7.jpg')
+const img118 = require('@/assets/gqyxgq/gqyxgq8.jpg')
+const img119 = require('@/assets/gqyxgq/gqyxgq9.jpg')
+const img120 = require('@/assets/gqyxgq/gqyxgq10.jpg')
+
+
+
+
+//泗州戏（蚌埠市）
+const img121 = require('@/assets/szxbbs/szxbbs1.jpg')
+const img122 = require('@/assets/szxbbs/szxbbs2.jpg')
+const img123 = require('@/assets/szxbbs/szxbbs3.jpg')
+const img124 = require('@/assets/szxbbs/szxbbs4.jpg')
+const img125 = require('@/assets/szxbbs/szxbbs5.jpg')
+const img126 = require('@/assets/szxbbs/szxbbs6.jpg')
+const img127 = require('@/assets/szxbbs/szxbbs7.jpg')
+const img128 = require('@/assets/szxbbs/szxbbs8.jpg')
+const img129 = require('@/assets/szxbbs/szxbbs9.jpg')
+const img130 = require('@/assets/szxbbs/szxbbs10.jpg')
+
+
+
+//徽州三雕
+const img231 = require('@/assets/huizhousandiao/huizhousandiao1.jpg')
+
+
+
+//万安罗盘制作技艺
+const img241 = require('@/assets/wananluopanzhizuojiyi/wananluopanzhizuojiyi1.jpg')
+
+
+
+
+//歙砚制作技艺
+const img251 = require('@/assets/sheyanzhizuojiyi/sheyanzhizuojiyi1.jpg')
+
+
+
+//徽墨制作技艺
+const img261 = require('@/assets/huimozhizuojiyi/huimozhizuojiyi1.jpg')
+
+
+
+//徽墨制作技艺（黄山市歙县）
+const img271 = require('@/assets/shexianhmzzjy/shexianhmzzjy1.png')
+
+
+
+//芜湖铁画锻制技艺
+const img281 = require('@/assets/wuhutiehua/wuhutiehua1.jpg')
+
+
+
+//花鼓灯（蚌埠市）
+const img291 = require('@/assets/bbhgd/bbhgd1.jpg')
+
+
+
+//宣纸制作技艺
+const img2101 = require('@/assets/xzzzjy/xzzzjy1.jpg')
+
+
+
+//花鼓灯（阜阳市颍上县）
+const img2111 = require('@/assets/fyhgd/fyhgd1.jpg')
+
+
+
+//花鼓灯（淮南市凤台县）
+const img2121 = require('@/assets/hnhgd/hnhgd1.jpg')
+
+
+
+//黄梅戏（安庆市）
+const img2131 = require('@/assets/aqhmx/aqhmx1.jpg')
+
+
+
+//傩戏（池州傩戏）
+const img2141 = require('@/assets/chizhounuoxi/chizhounuoxi1.jpg')
+
+
+
+//青阳腔
+const img2151 = require('@/assets/qyj/qyj1.jpg')
+
+
+const imgs = {
+    '凤阳花鼓':[img1,img1],
+    '徽墨制造工艺':[img11,img11,img11,img11,img11,img11],
+    '界首彩陶烧制技艺':[img21,img21,],
+    '目连戏（徽州目连戏）':[img31,img32,img33,img34,img35,img36,img37,img38,img39,img40,] ,
+    '徽剧（黄山市）':[img41,img42,img43,img44,img45,img46,img47,img48,img49,img50,],
+    '徽剧（省徽京剧院）':[img51,img52,img53,img54,img55,img56,img57,img58,img59,img60,],
+    '庐剧（合肥市）':[img61,img62,img63,img64,img65,img66,img67,img68,img69,img70,],
+    '庐剧（六安市）':[img71,img72,img73,img74,img75,img76,img77,img78,img79,img80,],
+    '泗州戏（宿州市）':[img81,img82,img83,img84,img85,img86,img87,img88,img89,img90,],
+    '巢湖民歌':[img91,img92,img93,img94,img95,img96,img97,img98,img99,img100,],
+    '当涂民歌':[img101,img102,img103,img104,img105,img106,img107,img108,img109,img110,],
+    '高腔（岳西高腔）':[img111,img112,img113,img114,img115,img116,img117,img118,img119,img120,],
+    '泗州戏（蚌埠市）':[img121,img122,img123,img124,img125,img126,img127,img128,img129,img130,],
+    '徽州三雕':[img231,img231,],
+    '万安罗盘制作技艺' : [img241,img241,], 
+    '歙砚制作技艺' : [img251,img251,],
+    '徽墨制作技艺' : [img261,img261,],
+    '徽墨制作技艺（黄山市歙县）' : [img271,img271,],
+    '芜湖铁画锻制技艺' : [img281,img281,],
+    '花鼓灯（蚌埠市）' : [img291,img291,],
+    '宣纸制作技艺' : [img2101,img2101,],
+    '花鼓灯（阜阳市颍上县）' : [img2111,img2111,],
+    '花鼓灯（淮南市凤台县）' : [img2121,img2121,],
+    '黄梅戏（安庆市）' : [img2131,img2131,],
+    '傩戏（池州傩戏）' : [img2141,img2141,],
+    '青阳腔' : [img2151,img2151,],
+
+
+}
+export default imgs
---
+++ src/views/goodsItem/goodsItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <DetailView :curIntangibleCulturalHeritage="curIntangibleCulturalHeritage" />
+  </div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+import DetailView from '../components/DetailView.vue'
+export default {
+  name: "HelloWorld",
+  components: {
+    DetailView
+  },
+
+  mounted() {
+    const route = useRoute(); // 引入 Route
+    this.curIntangibleCulturalHeritage = route.query.name
+
+  },
+  data() {
+    return {
+      // 数据
+      curIntangibleCulturalHeritage: '',
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
