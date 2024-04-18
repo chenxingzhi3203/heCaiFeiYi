6bea1debe9e0ec48443bb6a82c539b49559bafaf
chenxingzhi123 <7350216+chenxingzhi123@user.noreply.gitee.com>
2024-04-19 00:32:52 +0800

    664

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-
-  configureWebpack: {
-    devtool: 'source-map', // 添加这行
-  },
-})
